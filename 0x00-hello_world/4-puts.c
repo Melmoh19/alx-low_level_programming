e7e503dfa5272a0190a743660421f2c1d7316d1f
Melmoh19 <andersonhart003@gmail.com>
2024-06-24 07:33:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main - A program that prints a line with puts function
- * return: 0 (Success)
+ * Return: 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }

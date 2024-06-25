b72f34011faea544fd964dd0e6795ee1eb8478f5
Melmoh19 <andersonhart003@gmail.com>
2024-06-24 17:18:51 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+/**
+ *main - the start point of the program
+ *
+ * Description: The function uses for loop to display digits and comma
+ *
+ * Return: the result is 0 to indicate success.
+ */
+int main(void)
+{
+	int i;
+	int n;
 
-int main(void) {
-    for (int i = 0; i < 10; i++) {
-        for (int j = i + 1; j < 10; j++) {
-            putchar(i + '0');
-            putchar(j + '0');
-            if (i != 8 || j != 9) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return 0;
+	for (i = 0; i <= 9; i++)
+	{
+		for (n = i + 1; n <= 9; n++)
+		{
+			putchar(i + '0');
+			putchar(n + '0');
+			if (i < 8 || n < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,34 +2,30 @@
 
 /**
  * main - prints all possible different combinations of three digits
- *
- * Description: Numbers must be separated by commas and a space.
- * The three digits must be different.
- * 012, 120, 102, 021, 201, 210 are considered the same combination.
- * Print only the smallest combination of three digits.
- * Numbers should be printed in ascending order, with three digits.
- *
+ * Description: Numbers are separated by commas and spaces.
+ * The three digits are different.
+ * Only the smallest combination of three digits is printed.
+ * Numbers are printed in ascending order.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i <= 7; i++)
 	{
-		for (j = i + 1; j < 9; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = j + 1; k < 10; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
-				putchar((i % 10) + '0');
-				putchar((j % 10) + '0');
-				putchar((k % 10) + '0');
-
-				if (i == 7 && j == 8 && k == 9)
-					continue;
-
-				putchar(',');
-				putchar(' ');
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}

efd66ee842e745fd8fd128937e17f6ec306e259a
Melmoh19 <andersonhart003@gmail.com>
2024-06-24 17:03:35 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,21 +1,15 @@
-int main(void)
-{
+#include <stdio.h>
+
+int main(void) {
     int i, j, k;
 
-    i = -1;
-    while (++i < 8)
-    {
-        j = i;
-        while (++j < 9)
-        {
-            k = j;
-            while (++k < 10)
-            {
+    for (i = 0; i < 10; i++) {
+        for (j = i + 1; j < 10; j++) {
+            for (k = j + 1; k < 10; k++) {
                 putchar(i + '0');
                 putchar(j + '0');
                 putchar(k + '0');
-                if (i != 7 || j != 8 || k != 9)
-                {
+                if (i != 7 || j != 8 || k != 9) {
                     putchar(',');
                     putchar(' ');
                 }
@@ -23,5 +17,5 @@ int main(void)
         }
     }
     putchar('\n');
-    return (0);
+    return 0;
 }

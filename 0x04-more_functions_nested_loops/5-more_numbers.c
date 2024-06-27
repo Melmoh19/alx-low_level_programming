a46a35407e7eb82a49f0e196ba87af73d584bc32
Melmoh19 <andersonhart003@gmail.com>
2024-06-27 15:37:18 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - the main of the program
+ *
+ * Description: print 1 - 14
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}

e89808f4d20aea612676f8654b28ffa4c36ab498
Melmoh19 <andersonhart003@gmail.com>
2024-06-27 15:46:50 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_diagonal - main function
+ *
+ * @n: is the check character
+ *
+ * Description: draw diagonal lines
+ *
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
